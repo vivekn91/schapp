f9e5f6b456840345942ca18e2e89375052eb6755
vivek.n91 <vivek.check91@gmail.com>
2018-05-12 21:14:57 +0530

    homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from "@angular/router";
+
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { MainComponent } from './partials/main/main.component';
+import { HomeComponent } from './partials/home/home.component';
+import { AboutComponent } from './partials/about/about.component';
+import { ContactComponent } from './partials/contact/contact.component';
+import { AcademicsComponent } from './partials/academics/academics.component';
+
+
+const routes: Routes = [
+ { path: '', component: HomeComponent },
+ { path: 'home', component: HomeComponent },
+ { path: 'about', component: AboutComponent }
+ { path: 'academics', component: AcademicsComponent }
+ { path: 'contact', component: ContactComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    AcademicsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
